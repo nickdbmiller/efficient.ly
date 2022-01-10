184b943ce7d234e6203ffb5db926e1b5c3634f88
nickdbmiller <nickbarrettmiller@gmail.com>
2022-01-10 18:17:46 -0500

    Add links to info categories.

--- client/efficient.ly/src/components/Info.jsx
+++ client/efficient.ly/src/components/Info.jsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import api from "../services/apiConfig";
 
 export default function Info() {
+    const [infoData, setInfoData] = useState([]);
+
+    useEffect(() => {
+        const fetchInfo = async() => {
+            const res = await api.get('infoData');
+            setInfoData(res.data.records);
+        };
+        fetchInfo();
+    }, []);
+
     return (
         <div>
-            Info
+            <h2>Choose a category:</h2>
+            <ul>
+                {infoData.map((info) => {
+                    return (
+                        <li key={info.id}>
+                            <Link to={`/info/${info.id}`}>
+                                <h3>{info?.fields.category}</h3>
+                            </Link>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     )
 }

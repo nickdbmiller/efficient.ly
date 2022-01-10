40faf0b7b55ed6bdeb141d3b5326b3cc06bd69a6
nickdbmiller <nickbarrettmiller@gmail.com>
2022-01-10 15:45:17 -0500

    Add heading and text.

--- client/efficient.ly/src/components/Home.jsx
+++ client/efficient.ly/src/components/Home.jsx
@@ -2,7 +2,9 @@
 export default function Home() {
     return (
         <div>
-            Home
+            <h2>Welcome Home</h2>
+            <p>efficient.ly empowers your decision making about your home.</p>
+            <p>Here you can learn ways to make your home more efficient, save money, and protect the planet. Click a link in the navbar above to get started!</p>
         </div>
     )
 }

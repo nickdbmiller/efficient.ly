6d4d97e12800d47a8fa812aa16fcebd10d3bfe88
nickdbmiller <nickbarrettmiller@gmail.com>
2022-01-10 14:56:58 -0500

    Add routes to home, info, tools, saved, and error.

--- client/efficient.ly/src/App.js
+++ client/efficient.ly/src/App.js
@@ -3,12 +3,24 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import Header from './components/Header';
+import Home from './components/Home';
+import Info from './components/Info';
+import Tools from './components/Tools';
+import Saved from './components/Saved';
+import Error from './components/Error';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/info" element={<Info />} />
+        <Route path="/tools" element={<Tools />} />
+        <Route path="/saved" element={<Saved />} />
+        <Route path="/:undefined" element={<Error />} />
+      </Routes>
       <Footer />
     </div>
   );

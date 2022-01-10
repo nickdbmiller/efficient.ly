cb70a163ef32598449291eae16eeab9b8588bebb
nickdbmiller <nickbarrettmiller@gmail.com>
2022-01-10 15:04:07 -0500

    Add links to home, info, tools, and saved.

--- client/efficient.ly/src/components/Navbar.jsx
+++ client/efficient.ly/src/components/Navbar.jsx
@@ -1,12 +1,21 @@
+import { Link } from "react-router-dom"
 
 export default function Navbar() {
     return (
         <nav>
             <ul>
-            <li>Home</li>
-            <li>Info</li>
-            <li>Tools</li>
-            <li>Saved</li>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/info">Info</Link>
+                </li>
+                <li>
+                    <Link to="/tools">Tools</Link>
+                </li>
+                <li>
+                    <Link to="/saved">Saved</Link>
+                </li>
             </ul>
         </nav>
     )

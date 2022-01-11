43d3637249d3002bbe0989660e55f60f14b1eea5
nickdbmiller <nickbarrettmiller@gmail.com>
2022-01-11 10:23:47 -0500

    Add route to specific tool.

--- client/efficient.ly/src/App.js
+++ client/efficient.ly/src/App.js
@@ -6,7 +6,8 @@ import Header from './components/Header';
 import Home from './components/Home';
 import Info from './components/Info';
 import InfoDetail from './components/InfoDetail';
-import Tools from './components/Tools';
+import ToolList from './components/ToolList';
+import Tool from './components/Tool';
 import Saved from './components/Saved';
 import Error from './components/Error';
 import Footer from './components/Footer';
@@ -19,7 +20,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/info" element={<Info />} />
         <Route path="/info/:id" element={<InfoDetail />} />
-        <Route path="/tools" element={<Tools />} />
+        <Route path="/tools" element={<ToolList />} />
+        <Route path="/tools/:id" element={<Tool />} />
         <Route path="/saved" element={<Saved />} />
         <Route path="/:undefined" element={<Error />} />
       </Routes>

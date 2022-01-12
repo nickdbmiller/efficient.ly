d68b0bf0ab19f24ac06ba333fa08605566959bc9
nickdbmiller <nickbarrettmiller@gmail.com>
2022-01-12 15:45:15 -0500

    Add list of saved items

--- client/efficient.ly/src/components/Saved.jsx
+++ client/efficient.ly/src/components/Saved.jsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import api from "../services/apiConfig";
 
 export default function Saved() {
+    const [saveData, setSaveData] = useState([]);
+
+    useEffect(() => {
+        const fetchSaveData = async() => {
+            const res = await api.get('heatingData');
+            console.log(res);
+            setSaveData(res.data.records);
+        };
+        fetchSaveData();
+    }, []);
+
     return (
         <div>
-            Saved
+            <h2>Saved Data:</h2>
+            <ul>
+                {saveData.map((house) => {
+                    return (
+                        <li key={house.id}>
+                                <h3>{`${house?.fields.climateZone}`}</h3>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     )
 }

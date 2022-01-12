9e16c800f1dd4662a9b7143d48b115f501804d6a
nickdbmiller <nickbarrettmiller@gmail.com>
2022-01-12 15:41:18 -0500

    Remove console.logs.

--- client/efficient.ly/src/components/ToolList.jsx
+++ client/efficient.ly/src/components/ToolList.jsx
@@ -8,7 +8,6 @@ export default function ToolList() {
     useEffect(() => {
         const fetchTools = async() => {
             const res = await api.get('tools');
-            console.log(res);
             setToolData(res.data.records);
         };
         fetchTools();

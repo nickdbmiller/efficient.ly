a0eacc9a148ddee087f6cba56574ab60a9dc1157
nickdbmiller <nickbarrettmiller@gmail.com>
2022-01-14 12:17:19 -0500

    Refactor for readability. Add rel=noreferrer to a tags.

--- client/efficient.ly/src/components/Footer.jsx
+++ client/efficient.ly/src/components/Footer.jsx
@@ -4,8 +4,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 export default function Footer() {
     return (
         <footer>
-            <span className = "footer-icon"><a href="https://www.linkedin.com/in/ndbmiller" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a></span>
-            <span className = "footer-icon"><a href="https://github.com/nickdbmiller" target="_blank"><FontAwesomeIcon icon={faGithubSquare} /></a></span>
+            <span className = "footer-icon">
+                <a href="https://www.linkedin.com/in/ndbmiller" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+            </span>
+            <span className = "footer-icon">
+                <a href="https://github.com/nickdbmiller" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faGithubSquare} />
+                </a>
+            </span>
             <div>🄯 Nicholas Barrett-Miller 2022</div>
         </footer>
     )

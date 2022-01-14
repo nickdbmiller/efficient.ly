ca48ccf8a13331e9094926b50b03fb02fa7db622
nickdbmiller <nickbarrettmiller@gmail.com>
2022-01-14 11:04:57 -0500

    Add darkmode, and variant extensions to config.

--- client/efficient.ly/tailwind.config.js
+++ client/efficient.ly/tailwind.config.js
@@ -2,8 +2,15 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
+  purge: [],
+  darkMode: false,
   theme: {
     extend: {},
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["active"],      
+    },
   },
   plugins: [],
 }

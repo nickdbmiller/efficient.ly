3f6170502983e4649258deeb0a96ab5bb38a9753
nickdbmiller <nickbarrettmiller@gmail.com>
2022-01-15 10:37:06 -0500

    Hover and active styling for header link.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,9 +2,26 @@ import Navbar from "./Navbar"
 
 export default function Header() {
     return (
-        <header>
-            <a href="https://github.com/nickdbmiller/efficient.ly" target="_blank" rel="noreferrer">
-                <h1>efficient.ly</h1>
+        <header className="flex justify-between items-center bg-lime-900 px-1 py-4">
+            <a
+                className="flex text-lime-400 text-2xl
+                hover:text-lime-300 hover:-translate-y-0.5 transform transition
+                focus:outline-none focus:ring focus:ring-offset-2 focus:ring-lime-400 focus:ring-opacity-50
+                active:text-lime-500"
+                href="https://github.com/nickdbmiller/efficient.ly"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img
+                    className="h-10"
+                    src="../../favicon.ico"
+                    alt="efficient.ly lightning bolt logo"
+                />
+                <h1 
+                    className=""
+                >
+                    efficient.ly
+                </h1>
             </a>
             <Navbar />
         </header>

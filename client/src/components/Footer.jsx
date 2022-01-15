c3e05b661c9fd6dfda6395351932d2161a462649
nickdbmiller <nickbarrettmiller@gmail.com>
2022-01-15 11:33:20 -0500

    Style footer link icons.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,18 +3,28 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export default function Footer() {
     return (
-        <footer>
-            <span className = "footer-icon">
-                <a href="https://www.linkedin.com/in/ndbmiller" target="_blank" rel="noreferrer">
+        <footer className="flex flex-col justify-between items-center bg-lime-900 px-1 py-4">
+            <div className="flex">
+                <a
+                    className = "mb-2 mx-4 rounded-lg text-lime-400 text-3xl hover:text-lime-300 hover:-translate-y-0.5 transform transition focus:outline-none focus:ring focus:ring-offset-2 focus:ring-lime-400 focus:ring-opacity-50 active:text-lime-500"
+                    href="https://www.linkedin.com/in/ndbmiller"
+                    target="_blank"
+                    rel="noreferrer"
+                >
                     <FontAwesomeIcon icon={faLinkedin} />
                 </a>
-            </span>
-            <span className = "footer-icon">
-                <a href="https://github.com/nickdbmiller" target="_blank" rel="noreferrer">
+
+                <a
+                    className = "mb-2 mx-4 rounded-lg text-lime-400 text-3xl hover:text-lime-300 hover:-translate-y-0.5 transform transition focus:outline-none focus:ring focus:ring-offset-2 focus:ring-lime-400 focus:ring-opacity-50 active:text-lime-500"
+                    href="https://github.com/nickdbmiller"
+                    target="_blank"
+                    rel="noreferrer"
+                >
                     <FontAwesomeIcon icon={faGithubSquare} />
                 </a>
-            </span>
-            <div>🄯 Nicholas Barrett-Miller 2022</div>
+            </div>
+
+            <div className="text-lime-1000">🄯 Nicholas Barrett-Miller 2022</div>
         </footer>
     )
 }

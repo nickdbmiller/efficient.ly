30d9b75806ebbd685674d11b2d94cd4b42537e47
nickdbmiller <nickbarrettmiller@gmail.com>
2022-01-15 15:26:19 -0500

    Move barn and watercolor splash to its own component.

---
+++ client/src/components/BarnSplash.jsx
@@ -0,0 +1,8 @@
+export default function BarnSplash() {
+    return (
+        <div className="relative top-0 left-0">
+            <img className="h-100 relative top-0 left-0" src="https://i.imgur.com/UB2uELH.png" alt="green watercolor splash" />
+            <img className="h-100 absolute top-0 left-0" src="https://i.imgur.com/injgadx.png" alt="Watercolor Barn" title="Credit: Matt Fussell in Watercolor"/>
+        </div>
+    )
+}

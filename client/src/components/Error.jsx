13a7e3fe0961cad43c20b7080732cfe5b8feefbb
nickdbmiller <nickbarrettmiller@gmail.com>
2022-01-15 15:26:37 -0500

    Add BarnSplash component.

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -1,11 +1,10 @@
+import BarnSplash from "./BarnSplash";
+
 export default function Error() {
     return (
         <div>
             <h2 className="text-center text-2xl mt-24 text-lime-700">404: Data Not Found</h2>
-            <div className="relative top-0 left-0">
-                <img className="h-100 relative top-0 left-0" src="https://i.imgur.com/UB2uELH.png" alt="green watercolor splash" />
-                <img className="h-100 absolute top-0 left-0" src="https://i.imgur.com/injgadx.png" alt="barn" />
-            </div>
+            <BarnSplash />
         </div>
     )
 }
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,3 +1,5 @@
+import BarnSplash from "./BarnSplash"
+
 export default function Home() {
     return (
         <div>
@@ -7,10 +9,7 @@ export default function Home() {
                 Welcome <span className="text-lime-700">Home</span>
             </h2>
             <p className="mt-6">efficient.ly empowers your decision making about your home.</p>
-            <div className="relative top-0 left-0">
-                <img className="h-100 relative top-0 left-0" src="https://i.imgur.com/UB2uELH.png" alt="green watercolor splash" />
-                <img className="h-100 absolute top-0 left-0" src="https://i.imgur.com/injgadx.png" alt="barn" />
-            </div>
+            <BarnSplash />
             <p className="mt-6">Here you can learn ways to make your home more efficient, save money, and protect the planet.</p>
         </div>
     )

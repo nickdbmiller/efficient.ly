8d24cc2c0b2e548e0cf91bb8adc75f55285a0f6e
nickdbmiller <nickbarrettmiller@gmail.com>
2022-01-15 11:05:49 -0500

    Shaddow styling for header.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,9 +2,9 @@ import Navbar from "./Navbar"
 
 export default function Header() {
     return (
-        <header className="flex justify-between items-center bg-lime-900 px-1 py-4">
+        <header className="flex justify-between items-center bg-lime-900 px-1 py-4 shadow-lg shadow-lime-900">
             <a
-                className="flex text-lime-400 text-2xl
+                className="flex justify-between items-center rounded-lg text-lime-400 text-2xl
                 hover:text-lime-300 hover:-translate-y-0.5 transform transition
                 focus:outline-none focus:ring focus:ring-offset-2 focus:ring-lime-400 focus:ring-opacity-50
                 active:text-lime-500"
@@ -15,7 +15,7 @@ export default function Header() {
                 <img
                     className="h-10"
                     src="../../favicon.ico"
-                    alt="efficient.ly lightning bolt logo"
+                    alt="efficient.ly lightning bolt logo."
                 />
                 <h1 
                     className=""

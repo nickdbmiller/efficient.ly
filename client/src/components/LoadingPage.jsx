333ac3dc2c903e2e669dac5e293c7f317f431ade
nickdbmiller <nickbarrettmiller@gmail.com>
2022-01-15 13:55:54 -0500

    Add styling to loading page.

--- client/src/components/LoadingPage.jsx
+++ client/src/components/LoadingPage.jsx
@@ -1,7 +1,11 @@
 export default function LoadingPage() {
     return (
         <div>
-            <h2>Loading...</h2>
+            <h2 className="text-center text-2xl mt-24 text-lime-700">Loading...</h2>
+            <div className="relative top-0 left-0">
+                <img className="h-100 relative top-0 left-0" src="https://i.imgur.com/UB2uELH.png" alt="green watercolor splash" />
+                <img className="h-100 absolute top-0 left-0" src="https://i.imgur.com/injgadx.png" alt="barn" />
+            </div>
         </div>
     )
 }

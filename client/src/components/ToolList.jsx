9422a24a90a8368a2c612082b5da732a3265e6f3
nickdbmiller <nickbarrettmiller@gmail.com>
2022-01-16 10:30:36 -0500

    Style list of tools.

--- client/src/components/ToolList.jsx
+++ client/src/components/ToolList.jsx
@@ -21,14 +21,21 @@ export default function ToolList() {
 
     return (
         <div>
-            <h2>Choose a tool:</h2>
-            <ul>
+            <h2 className="text-xl">Choose a <span className="text-lime-700 font-bold">tool</span>:</h2>
+            <ul className="text-center bg-lime-700 px-6 pt-6 m-6 drop-shadow-2xl rounded-lg">
                 {toolData.map((tool) => {
                     return (
                         <li key={tool.id}>
                             <Link to={`/tools/${tool?.fields.toolName}`}>
-                                <h3>{`${tool?.fields.toolUpperCase}`}</h3>
+                                <h3
+                                    className="font-bold text-lime-400 hover:text-lime-300 hover:-translate-y-0.5 transform transition
+                                    focus:outline-none focus:ring focus:ring-lime-400 focus:ring-opacity-50
+                                    active:text-lime-500"
+                                >
+                                    {`${tool?.fields.toolUpperCase}`}
+                                </h3>
                             </Link>
+                            <br/>
                         </li>
                     )
                 })}

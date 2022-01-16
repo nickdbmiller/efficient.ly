4a0a05ed1630ef1be09a8d206a7d7c753d6c233d
nickdbmiller <nickbarrettmiller@gmail.com>
2022-01-16 10:31:42 -0500

    Add bold font for h1 span.

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
             <h2 
                 className="text-xl"
             >
-                Welcome <span className="text-lime-700">Home</span>
+                Welcome <span className="text-lime-700 font-bold">Home</span>
             </h2>
             <p className="mt-6">efficient.ly empowers your decision making about your home.</p>
             <BarnSplash />

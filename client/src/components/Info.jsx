4a0d5d2ff51eab8800d969181700c13669e93291
nickdbmiller <nickbarrettmiller@gmail.com>
2022-01-16 20:31:30 -0500

    Add bottom border, change top margin, remove line break.

--- client/src/components/Info.jsx
+++ client/src/components/Info.jsx
@@ -22,20 +22,19 @@ export default function Info() {
     return (
         <div>
             <h2 className="text-xl">Choose a <span className="text-lime-700 font-bold">category</span>:</h2>
-            <ul className="text-center bg-lime-700 px-6 pt-6 m-6 drop-shadow-2xl rounded-lg">
+            <ul className="text-center bg-lime-700 px-6 py-3 m-6 drop-shadow-2xl rounded-lg border-b-2 border-lime-1000">
                 {infoData.map((info) => {
                     return (
                         <li key={info.id}>
                             <Link to={`/info/${info?.id}`}>
                                 <h3
-                                    className="font-bold text-lime-400 hover:text-lime-300 hover:-translate-y-0.5 transform transition
+                                    className="my-3 inline-block font-bold text-lime-400 hover:text-lime-300 hover:-translate-y-0.5 transform transition
                                     focus:outline-none focus:ring focus:ring-lime-400 focus:ring-opacity-50
                                     active:text-lime-500"
                                 >
                                     {info?.fields.category}
                                 </h3>
                             </Link>
-                            <br/>
                         </li>
                     )
                 })}

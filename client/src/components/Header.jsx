c48de64d27914e18b6a12817eef3a3367314bd79
nickdbmiller <nickbarrettmiller@gmail.com>
2022-01-16 20:30:07 -0500

    Add bottom border.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,7 @@ import Navbar from "./Navbar"
 
 export default function Header() {
     return (
-        <header className="sticky top-0 z-50 flex justify-between items-center bg-lime-900 px-1 py-4 shadow-xl">
+        <header className="sticky top-0 z-50 flex justify-between items-center bg-lime-900 px-1 py-4 shadow-xl border-b-2 border-lime-1000">
             <a
                 className="flex justify-between items-center rounded-lg text-lime-400 text-2xl font-bold
                 hover:text-lime-300 hover:-translate-y-0.5 transform transition

9e3709471ede67211f9d1eee247b6f7169cb252f
nickdbmiller <nickbarrettmiller@gmail.com>
2022-01-17 14:46:44 -0500

    Horizontal navbar display.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,9 +2,9 @@ import Navbar from "./Navbar"
 
 export default function Header() {
     return (
-        <header className="sticky top-0 z-50 flex justify-between items-center bg-lime-900 px-1 py-4 shadow-xl border-b-2 border-lime-1000">
+        <header className="sticky top-0 z-50 flex-col justify-start bg-lime-900 px-4 pt-4 shadow-xl border-b-2 border-lime-1000">
             <a
-                className="flex justify-between items-center rounded-lg text-lime-400 text-2xl font-bold
+                className="flex flex-row rounded-lg text-lime-400 text-2xl font-bold
                 hover:text-lime-300 hover:-translate-y-0.5 transform transition
                 focus:outline-none focus:ring focus:ring-offset-2 focus:ring-lime-400 focus:ring-opacity-50
                 active:text-lime-500"
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,18 +2,34 @@ import { Link } from "react-router-dom"
 
 export default function Navbar() {
     return (
-        <nav>
-            <ul>
-                <li>
+        <nav className="text-lg px-4 py-4">
+            <ul className="flex justify-between text-lime-400">
+                <li
+                    className="hover:text-lime-300 hover:-translate-y-0.5 transform transition
+                    focus:outline-none focus:ring focus:ring-offset-2 focus:ring-lime-400 focus:ring-opacity-50
+                    active:text-lime-500"
+                >
                     <Link to="/">Home</Link>
                 </li>
-                <li>
+                <li
+                    className="hover:text-lime-300 hover:-translate-y-0.5 transform transition
+                    focus:outline-none focus:ring focus:ring-offset-2 focus:ring-lime-400 focus:ring-opacity-50
+                    active:text-lime-500"
+                >
                     <Link to="/info">Info</Link>
                 </li>
-                <li>
+                <li
+                    className="hover:text-lime-300 hover:-translate-y-0.5 transform transition
+                    focus:outline-none focus:ring focus:ring-offset-2 focus:ring-lime-400 focus:ring-opacity-50
+                    active:text-lime-500"
+                >
                     <Link to="/tools">Tools</Link>
                 </li>
-                <li>
+                <li
+                    className="hover:text-lime-300 hover:-translate-y-0.5 transform transition
+                    focus:outline-none focus:ring focus:ring-offset-2 focus:ring-lime-400 focus:ring-opacity-50
+                    active:text-lime-500"
+                >
                     <Link to="/saved">Saved</Link>
                 </li>
             </ul>

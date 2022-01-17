1f9b6a849af1df7c7ab14e9016c5bce50f55dc8b
nickdbmiller <nickbarrettmiller@gmail.com>
2022-01-16 20:28:51 -0500

    Style 404 lime-700.

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -3,7 +3,7 @@ import BarnSplash from "./BarnSplash";
 export default function Error() {
     return (
         <div>
-            <h2 className="text-center text-2xl mt-24 text-lime-700">404: Data Not Found</h2>
+            <h2 className="text-center text-2xl mt-24"><span className="text-lime-700 font-bold">404</span>: Data Not Found</h2>
             <BarnSplash />
         </div>
     )

941c96a304c7830dc7e4611b792dc817e89d097a
nickdbmiller <nickbarrettmiller@gmail.com>
2022-01-17 14:21:06 -0500

    List items display inline-block.

--- client/src/components/ToolList.jsx
+++ client/src/components/ToolList.jsx
@@ -22,13 +22,13 @@ export default function ToolList() {
     return (
         <div>
             <h2 className="text-xl">Choose a <span className="text-lime-700 font-bold">tool</span>:</h2>
-            <ul className="text-center bg-lime-700 px-6 pt-6 m-6 drop-shadow-2xl rounded-lg border-b-2 border-lime-1000">
+            <ul className="text-center bg-lime-700 px-6 py-6 m-6 drop-shadow-2xl rounded-lg border-b-2 border-lime-1000">
                 {toolData.map((tool) => {
                     return (
                         <li key={tool.id}>
                             <Link to={`/tools/${tool?.fields.toolName}`}>
                                 <h3
-                                    className="font-bold text-lime-400 hover:text-lime-300 hover:-translate-y-0.5 transform transition
+                                    className="inline-block font-bold text-lime-400 hover:text-lime-300 hover:-translate-y-0.5 transform transition
                                     focus:outline-none focus:ring focus:ring-lime-400 focus:ring-opacity-50
                                     active:text-lime-500"
                                 >
